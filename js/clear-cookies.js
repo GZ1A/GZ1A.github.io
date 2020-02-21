48a813f18e4f87b54ce85fff16b0d6f8c3b01b7e
GZ1A <GZ1A@outlook.com>
2020-02-22 04:29:38 +0900

    new blog 加特效 & untrack public

---
+++ js/clear-cookies.js
@@ -0,0 +1,13 @@
+// https://blog.csdn.net/shua67/article/details/81535883
+function clearCookie() {
+  var keys = document.cookie.match(/[^ =;]+(?=\=)/g);
+  if (keys) {
+    for (var i = keys.length; i--;) {
+      document.cookie = keys[i] + '=0;path=/;expires=' + new Date(0).toUTCString(); //清除当前域名下的,例如：m.kevis.com
+      document.cookie = keys[i] + '=0;path=/;domain=' + document.domain + ';expires=' + new Date(0).toUTCString(); //清除当前域名下的，例如 .m.kevis.com
+      document.cookie = keys[i] + '=0;path=/;domain=kevis.com;expires=' + new Date(0).toUTCString(); //清除一级域名下的或指定的，例如 .kevis.com
+    }
+  }
+  $("#divcookie").html(document.cookie);
+  alert('已清除 cookies');
+}
---
+++ js/click-counter.js
@@ -0,0 +1,49 @@
+function setCookie(cname, cvalue, exdays, path) {
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  var expires = "expires=" + d.toGMTString();
+  var path = "path=" + path;
+  document.cookie = cname + "=" + cvalue + "; " + expires + "; " + path;
+}
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var ca = document.cookie.split(';');
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i].trim();
+    if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
+  }
+  return "";
+}
+
+// init
+let cookie_count = getCookie('click_count');
+var _click_count = cookie_count == undefined ? 0 : cookie_count;
+console.log("_click_count = " + _click_count);
+
+// click event
+$("body").bind("click", function (e) { //直接给body一个事件好了.
+  var $i = $("<t>").text("+" + (++_click_count) + "s"); //添加到页面的元素
+  var x = e.pageX,
+    y = e.pageY; //鼠标点击的位置
+  $i.css({
+    "z-index": 99999,
+    "top": y - 15,
+    "left": x,
+    "position": "absolute",
+    "color": "0,0,0",
+    "opacity": 0.8
+  });
+  $("body").append($i);
+  $i.animate({
+      "top": y + 120,
+      "opacity": 0
+    },
+    1000,
+    function () {
+      $i.remove();
+    }
+  );
+  e.stopPropagation();
+  setCookie('click_count', _click_count, 36500, '/')
+});
