3786492c7e7cd18d5b16e10e4693d52ba5e2225a
GZ1A <GZ1A@outlook.com>
2020-02-23 04:05:47 +0900

    update click-counter.js

--- js/click-counter.js
+++ js/click-counter.js
@@ -1,4 +1,4 @@
-function setCookie(cname, cvalue, exdays, path, domain) {
+function setCookie(cname, cvalue, exdays, path = "", domain = "") {
   var d = new Date();
   d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
   var expires = "expires=" + d.toGMTString();
